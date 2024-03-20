7703245bc1fa4cef86bb17dae36959ca6470cc99
Lotfi <boutchacha.lotfi@gmail.com>
2024-03-20 19:12:11 +0100

    Add font style update

--- src/AttrsMenu/TextAttributes.js
+++ src/AttrsMenu/TextAttributes.js
@@ -2,18 +2,39 @@ import React, { useEffect, useState } from 'react';
 import { MDBBtn, MDBIcon, MDBInput } from "mdb-react-ui-kit";
 
 const TextAttributes = ({ element, updateAttribute }) => {
-  const [strokeWidth, setStrokeWidth] = useState(0);
   const [fontSize, setFontSize] = useState(0);
   const [fontFamily, setFontFamily] = useState('');
+  const [textAlign, setTextAlign] = useState('left');
+  const [fontStyle, setFontStyle] = useState([]);
 
   useEffect(() => {
     if (element) {
       // Update input values whenever element attributes change
-      setStrokeWidth(element.attrs.strokeWidth || 0);
       setFontSize(element.attrs.fontSize || 0);
       setFontFamily(element.attrs.fontFamily || 0);
+      setTextAlign(element.attrs.align || 'left');
+      setFontStyle(element.attrs.fontStyle?.split(' ') || []);
     }
   }, [element]);
+
+  const updateTextAlign = (align) => {
+    setTextAlign(align);
+    updateAttribute('align', align);
+  }
+
+  const updateFontStyle = (style) => {
+    const styleIndex = fontStyle.indexOf(style);
+    let newStyles = [];
+
+    if (styleIndex >= 0) {
+      // remove it
+      newStyles = fontStyle.filter(e => e !== style);
+    } else {
+      newStyles = [...fontStyle, style];
+    }
+    setFontStyle(newStyles);
+    updateAttribute('fontStyle', newStyles.join(' '));
+  }
 
   return (
     <>
@@ -50,14 +71,23 @@ const TextAttributes = ({ element, updateAttribute }) => {
         <MDBIcon fas icon="square" size="lg" className="d-inline-block" style={{color: element?.attrs?.fill}}/>
       </MDBBtn>
       <div className="d-flex p-2 switch-group">
-        <MDBBtn className="shadow-0 active">
+        <MDBBtn className={`shadow-0 ${textAlign === 'left' && 'active'}`} onClick={() => updateTextAlign('left')}>
           <MDBIcon fas icon="align-left" size="lg" className="d-inline-block" />
         </MDBBtn>
-        <MDBBtn className="shadow-0">
+        <MDBBtn className={`shadow-0 ${textAlign === 'center' && 'active'}`} onClick={() => updateTextAlign('center')}>
           <MDBIcon fas icon="align-center" size="lg" className="d-inline-block" />
         </MDBBtn>
-        <MDBBtn className="shadow-0">
+        <MDBBtn className={`shadow-0 ${textAlign === 'right' && 'active'}`} onClick={() => updateTextAlign('right')}>
           <MDBIcon fas icon="align-right" size="lg" className="d-inline-block" />
+        </MDBBtn>
+      </div>
+
+      <div className="d-flex p-2 switch-group">
+        <MDBBtn className={`shadow-0 ${fontStyle.includes('bold') && 'active'}`} onClick={() => updateFontStyle('bold')}>
+          <MDBIcon fas icon="bold" size="lg" className="d-inline-block" />
+        </MDBBtn>
+        <MDBBtn className={`shadow-0 ${fontStyle.includes('italic') && 'active'}`} onClick={() => updateFontStyle('italic')}>
+          <MDBIcon fas icon="italic" size="lg" className="d-inline-block" />
         </MDBBtn>
       </div>
     </>
